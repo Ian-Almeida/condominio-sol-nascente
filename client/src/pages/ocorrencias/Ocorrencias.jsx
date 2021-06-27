1f365c881bf6ed0886ef255078a104c8533ba1a6
Ian-Almeida <ian.almeida@edu.univali.br>
2021-06-27 03:43:45 -0300

    isse#2

--- client/src/pages/ocorrencias/Ocorrencias.jsx
+++ client/src/pages/ocorrencias/Ocorrencias.jsx
@@ -1,6 +1,365 @@
+import { useState, useEffect } from 'react';
+import { Card, Table, Button, Modal, Form, Select, Input, Popconfirm } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import api from '../../api';
+import { getLoggedUser, openNotificationWithIcon } from '../../utils/utils';
+
+const { Option } = Select;
+const { TextArea } = Input;
+
 const Ocorrencias = () => {
+  const [registerModal, setRegisterModal] = useState(false);
+  const [updateModal, setUpdateModal] = useState(false);
+
+  const [registerForm] = Form.useForm();
+  const [updateForm] = Form.useForm();
+
+  const [tableItems, setTableItems] = useState([]);
+  const [condominios, setCondominios] = useState([]);
+  const [updatedOcorrencia, setUpdatedOcorrencia] = useState({});
+
+  const [userCondominio, setUserCondominio] = useState({});
+  const user = getLoggedUser();
+
+  const categoriasOptions = [
+    'Perturbação',
+    'Edifício',
+    'Animais',
+  ]
+
+  const setRegisterModalVisible = (state) => {
+    if(!state) registerForm.resetFields();
+    setRegisterModal(state);
+  }
+
+  const setUpdateModalVisible = (state) => {
+    if(!state) updateForm.resetFields();
+    setUpdateModal(state);
+  }
+
+  useEffect(() => {
+    async function getCondominioByUserId() {
+      try {
+        const response = await api.getCondominioByUserId(user.id);
+        if(response) {
+          setUserCondominio({...response.data});
+        }
+      } catch (err) {
+        setUserCondominio({});
+      }
+      
+    }
+    getCondominioByUserId()
+  }, [user.id]);
+
+  useEffect(() => {
+    async function getCondominios() {
+      try {
+        const response = await api.getCondominios();
+        if(response) {
+          const { data } = response;
+          setCondominios(data);
+        }
+      } catch (err) {
+        setCondominios([]);
+      }
+    }
+    getCondominios();
+  }, [])
+
+  useEffect(() => {
+    if(Object.keys(userCondominio).length === 0) return
+    async function getOcorrenciasByCondominioId() {
+      try {
+        const response = await api.getOcorrenciasByCondominioId(userCondominio.id);
+        if(response) {
+          const { data } = response;
+          setTableItems(data);
+        }
+      } catch (err) {
+        setTableItems([]);
+      }
+    }
+    getOcorrenciasByCondominioId();
+  }, [userCondominio])
+
+  const columns = [
+    {
+      title: 'Condomínio',
+      align: 'center',
+      dataIndex: 'nome_condominio',
+      key: 'nome_condominio',
+      render: (text, record) => (
+        <span>{text}</span>
+      )
+    },
+    {
+      title: 'Categoria',
+      align: 'center',
+      dataIndex: 'categoria',
+      key: 'categoria',
+      render: (text, record) => (
+        <span>{text}</span>
+      )
+    },
+    {
+      title: 'Descrição Completa',
+      align: 'center',
+      dataIndex: 'descricao_completa',
+      key: 'descricao_completa',
+      render: (text, record) => (
+        <span>{text}</span>
+      )
+    },
+    {
+      title: 'Ações',
+      align: 'center',
+      render: (text, record) => (
+        <div className="flex justify-center space-x-2">
+          <div className="hover:bg-gray-200 rounded-full">
+            <EditOutlined onClick={() => {
+              console.log(record)
+              updateForm.setFieldsValue({
+                codigo_condominio: record.codigo_condominio, 
+                categoria: record.categoria, 
+                descricao_completa: record.descricao_completa,
+              });
+              setUpdatedOcorrencia({...record});
+              setUpdateModalVisible(true);
+            }}></EditOutlined>
+          </div>
+          <div className="hover:bg-gray-200 rounded-full">
+            <Popconfirm
+              title={<span>Tem certeza que deseja deletar essa Ocorrência?</span>}
+              onConfirm={() => {
+                deleteOcorrencia(record);
+              }}
+              onCancel={() => {}}
+              okText="Sim"
+              cancelText="Não"
+            >
+              <DeleteOutlined></DeleteOutlined>
+            </Popconfirm>
+          </div>
+        </div>
+      )
+    },
+  ];
+
+  const deleteOcorrencia = async(values) => {
+    try {
+      const response = await api.deleteOcorrencia(values.id);
+      if(response) {
+        openNotificationWithIcon('success', 'bottomLeft', 'Ocorrência Deletada', 'Ocorrência deletada com sucesso!');
+        const {data} = await api.getOcorrenciasByCondominioId(values.codigo_condominio);
+        setTableItems(data);
+      }
+    } catch (err) {
+      if(err && err.response){ 
+				const message = err.response.data;
+				openNotificationWithIcon('error', 'bottomLeft', 'Ocorreu um problema!', message);
+        return
+      }
+      openNotificationWithIcon('error', 'bottomLeft', 'Ocorreu um problema!', 'Erro inesperado!');
+    }
+  }
+
+  const updateOcorrencia = async(state, values) => {
+    
+    const obj = {...updatedOcorrencia}
+    obj.codigo_condominio = values.codigo_condominio;
+    obj.categoria = values.categoria;
+    obj.descricao_completa = values.descricao_completa;
+    console.log(obj)
+
+    try {
+      const response = await api.updateOcorrencia(obj);
+      if(response) {
+        openNotificationWithIcon('success', 'bottomLeft', 'Ocorrência Atualizado', 'Ocorrência atualizado com sucesso!');
+        const {data} = await api.getOcorrenciasByCondominioId(obj.codigo_condominio);
+        setTableItems(data);
+        setUpdateModal(state);
+      }
+
+    } catch (err) {
+      if(err && err.response){ 
+				const message = err.response.data;
+				openNotificationWithIcon('error', 'bottomLeft', 'Ocorreu um problema!', message);
+        return
+      }
+      console.log(err)
+      openNotificationWithIcon('error', 'bottomLeft', 'Ocorreu um problema!', 'Erro inesperado!');
+    }
+  }
+
+  const registerOcorrencia = async(state, values) => {
+    const createdOcorrencia = {...values, codigo_usuario: user.id};
+    try {
+      const response = await api.registerOcorrencia(createdOcorrencia);
+      if(response) {
+        openNotificationWithIcon('success', 'bottomLeft', 'Ocorrência Registrado', 'Ocorrência registrado com sucesso!');
+        const {data} = await api.getOcorrenciasByCondominioId(createdOcorrencia.codigo_condominio);
+        setTableItems(data);
+        setRegisterModal(state);
+      }
+    } catch (err) {
+      if(err && err.response){ 
+				const message = err.response.data;
+				openNotificationWithIcon('error', 'bottomLeft', 'Ocorreu um problema!', message);
+        return
+      }
+      openNotificationWithIcon('error', 'bottomLeft', 'Ocorreu um problema!', 'Erro inesperado!');
+    }
+  }
+
   return (
-    <div>Ocorrencias</div>
+    <Card 
+    title={
+      <div>
+        <span className="float-left">OCORRÊNCIAS</span>
+        <div className="float-right">
+          <Button onClick={() => setRegisterModalVisible(true)} type="primary">Cadastrar Ocorrência</Button>
+        </div>
+      </div>
+    } 
+    bordered={true}
+    >
+      <Table 
+        rowKey="id" 
+        pagination={{position: ['none', 'bottomCenter'], pageSize: 5}} 
+        dataSource={(tableItems)} 
+        columns={columns}
+       />
+
+      <Modal
+        title="Registrar Ocorrência"
+        centered
+        visible={registerModal}
+        footer={null}
+        onCancel={() => setRegisterModalVisible(false)}
+       >
+         <Form
+          form={registerForm}
+          name="register"
+          onFinish={(values) => registerOcorrencia(false, values)}
+          scrollToFirstError
+        >
+          <Form.Item
+            name="codigo_condominio"
+            label="Condomínio"
+            rules={[
+              {
+                required: true,
+                message: "Por favor selecione o condomínio!",
+              },
+            ]}
+          >
+            <Select>
+              {condominios.map((item, idx) => {
+                return (
+                  <Option key={`condominio-${idx}`} value={item.id}>
+                    {item.nome_condominio}
+                  </Option>
+                )
+              })}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            key="categoria-input"
+            name="categoria"
+            label="Categoria"
+            rules={[
+              {
+                required: true,
+                message: "Por favor selecione a Categoria!",
+              },
+            ]}
+          >
+            <Select>
+              {categoriasOptions.map((item, idx) => {
+                return (
+                  <Option key={`categoria-${idx}`} value={item}>
+                    {item}
+                  </Option>
+                )
+              })}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            key="descricao-input"
+            name="descricao_completa"
+            label="Descrição Completa"
+            rules={[
+              {
+                required: true,
+                message: "Por favor digite a Descrição Completa!",
+              },
+            ]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="float-right">Concluir</Button>
+          </Form.Item>
+        </Form>
+       </Modal>
+
+       <Modal
+        title="Atualizar Ocorrência"
+        centered
+        visible={updateModal}
+        footer={null}
+        onCancel={() => setRegisterModalVisible(false)}
+       >
+         <Form
+          form={updateForm}
+          name="update"
+          onFinish={(values) => updateOcorrencia(false, values)}
+          scrollToFirstError
+        >
+          <Form.Item
+            name="codigo_condominio"
+            label="Condomínio"
+          >
+            <Select>
+              {condominios.map((item, idx) => {
+                return (
+                  <Option key={`condominio-${idx}`} value={item.id}>
+                    {item.nome_condominio}
+                  </Option>
+                )
+              })}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            key="categoria-input"
+            name="categoria"
+            label="Categoria"
+          >
+            <Select>
+              {categoriasOptions.map((item, idx) => {
+                return (
+                  <Option key={`categoria-${idx}`} value={item}>
+                    {item}
+                  </Option>
+                )
+              })}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            key="descricao-input"
+            name="descricao_completa"
+            label="Descrição Completa"
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="float-right">Concluir</Button>
+          </Form.Item>
+        </Form>
+       </Modal>
+
+    </Card>
   )
 }
 
--- server/api/routes/ocorrencias.js
+++ server/api/routes/ocorrencias.js
@@ -1,4 +1,95 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../database/deps');
+
+router.put('/', async (req, res) => {
+  const { body } = req;
+  console.log(body)
+  try {
+    const query = `
+    UPDATE 
+      ocorrencias 
+    SET 
+      codigo_condominio = ${body.codigo_condominio}, 
+      codigo_usuario = ${body.codigo_usuario}, 
+      categoria = '${body.categoria}', 
+      descricao_completa = '${body.descricao_completa}'
+    WHERE
+      id = ${body.id}
+    `;
+
+    const result = await db.execute(query);
+
+    if(!result) {
+      res.status(405).send('Erro ao atualizar objeto');
+    }
+
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+router.post('/', async (req, res) => {
+  const { body } = req;
+
+  try {
+    let query = `INSERT INTO ocorrencias (codigo_condominio, codigo_usuario, categoria, descricao_completa)
+      VALUES (?, ?, ?, ?)
+    `;
+    const params = [body.codigo_condominio, body.codigo_usuario, body.categoria, body.descricao_completa];
+    let ocorrencia = await db.execute(query, params);
+
+    if(!ocorrencia) {
+      res.status(404).send('Erro ao registrar Ocorrência!');
+    }
+
+    res.send(ocorrencia);
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+router.get('/:condominioId', async (req, res) => {
+  const { params } = req;
+
+  try {
+    let query = `
+      SELECT 
+        ocorrencias.*,
+        c.nome_condominio 
+      FROM 
+        ocorrencias
+        LEFT JOIN condominios c on c.id = ocorrencias.codigo_condominio
+      WHERE 
+        ocorrencias.codigo_condominio = ${params.condominioId}`;
+    let ocorrencias = await db.execute(query);
+
+    if(!ocorrencias) {
+      res.status(404).send('Ocorrências desse condomínio não encontrado!');
+    }
+
+    res.send(ocorrencias);
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const { params } = req;
+
+  try {
+    const query = `DELETE FROM ocorrencias WHERE id = ${params.id}`;
+    const result = await db.execute(query);
+
+    if(!result) {
+      res.status(405).send('Erro ao deletar Ocorrência');
+    }
+
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 module.exports = router;
